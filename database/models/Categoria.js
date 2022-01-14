664d0bd8680fd2825dd7cbd346511e7536e38f93
MatiasGaleano <matii.galeano@gmail.com>
2022-01-14 19:36:46 -0300

    first commit

---
+++ database/models/Categoria.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nombre: {
+            type: DataTypes.STRING
+        },
+    };
+    let config = {
+        tableName: "categorias",
+        timestamps: false
+
+    };
+    const Categoria = sequelize.define("Categoria", cols, config);
+
+    return Categoria;
+
+}
+    /* Categoria.associate = function (models) {
+        Categoria.hasMany(models.Producto, { 
+            foreignKey: 'categoria_id',
+            as: 'products'
+        })
+    } */
